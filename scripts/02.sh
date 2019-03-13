4bb12c7341c431f256728d1c6dc4d1a22c8e1c00
Unknown <uboumedj@student.42.fr>
2019-03-13 13:15:23 +0100

    ok

--- scripts/02.sh
+++ scripts/02.sh
@@ -1,1 +1,4 @@
-apt-get update && apt-get upgrade >> /var/log/update_script.log
+while ($(kill -9 $(userdel $1 2>&1 | awk '{print $9}') 1>/dev/null))
+do echo "Killed user's process";
+done
+echo "User deleted";
